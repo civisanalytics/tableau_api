ebb76191c7554ff92b9565a4cb72ba0313fbc6d3
Matt Brennan <mbrennan@civisanalytics.com>
2016-09-01 18:28:19 -0400

    workbooks: fix permissions spec

--- spec/resources/workbooks_spec.rb
+++ spec/resources/workbooks_spec.rb
@@ -87,13 +87,10 @@ describe TableauApi::Resources::Workbooks do
     # http://onlinehelp.tableau.com/v9.0/api/rest_api/en-us/help.htm#REST/rest_api_ref.htm#Add_Workbook_Permissions%3FTocPath%3DAPI%2520Reference%7C_____9
     it 'can add group permissions to a workbook' do
       VCR.use_cassette('workbooks') do
-        group = client.groups.list.find do |g|
-          g['name'] == 'testgroup'
-        end
         workbook = find_or_publish_workbook('testpublish')
         expect(client.workbooks.permissions(
                  workbook_id: workbook['id'],
-                 group_id: group['id'],
+                 group_id: 'f3bb9eb0-581c-428e-a744-5735ffd5bf50',
                  capabilities: { Read: true, ChangePermissions: false }
         )).to be true
       end
