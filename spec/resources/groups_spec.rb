f4b3afec173ec44e2b41c6a5e35ebbce35791975
Matt Brennan <mbrennan@civisanalytics.com>
2016-09-02 14:35:06 -0400

    spec: use rspec metadata to set VCR cassette when possible

--- spec/resources/groups_spec.rb
+++ spec/resources/groups_spec.rb
@@ -24,10 +24,7 @@ describe TableauApi::Resources::Groups do
     # http://onlinehelp.tableau.com/v9.0/api/rest_api/en-us/help.htm#REST/rest_api_ref.htm#Query_Groups
     it 'can list groups in a site' do
       VCR.use_cassette('groups') do
-        group = client.groups.list.find do |g|
-          g['name'] == 'testgroup'
-        end
-        expect(group['id']).to be_a_tableau_id
+        group = find_group_by_name('testgroup')
         expect(group).to eq('id' => group['id'], 'name' => 'testgroup', 'domain' => { 'name' => 'local' })
       end
     end
@@ -36,10 +33,7 @@ describe TableauApi::Resources::Groups do
   describe '#add_user' do
     it 'can add a user to a group' do
       VCR.use_cassette('groups') do
-        group = client.groups.list.find do |g|
-          g['name'] == 'testgroup'
-        end
-        expect(group['id']).to be_a_tableau_id
+        group = find_group_by_name('testgroup')
         expect(client.groups.add_user(group_id: group['id'], user_id: 'e1b91057-9cd9-4009-b6c9-cd18f1dc3fb4')).to be true
       end
     end
@@ -48,12 +42,17 @@ describe TableauApi::Resources::Groups do
   describe '#remove_user' do
     it 'can remove a user from a group' do
       VCR.use_cassette('groups') do
-        group = client.groups.list.find do |g|
-          g['name'] == 'testgroup'
-        end
-        expect(group['id']).to be_a_tableau_id
+        group = find_group_by_name('testgroup')
         expect(client.groups.remove_user(group_id: group['id'], user_id: 'e1b91057-9cd9-4009-b6c9-cd18f1dc3fb4')).to be true
       end
     end
+  end
+
+  def find_group_by_name(name)
+    group = client.groups.list.find do |g|
+      g['name'] == name
+    end
+    expect(group['id']).to be_a_tableau_id
+    group
   end
 end
--- spec/resources/sites_spec.rb
+++ spec/resources/sites_spec.rb
@@ -1,29 +1,27 @@
 require 'spec_helper'
 
-describe TableauApi::Resources::Sites do
+describe TableauApi::Resources::Sites, vcr: { cassette_name: 'sites' } do
   include_context 'tableau client'
 
   describe '#list' do
     # http://onlinehelp.tableau.com/v9.0/api/rest_api/en-us/help.htm#REST/rest_api_ref.htm#Query_Sites%3FTocPath%3DAPI%2520Reference%7C_____40
     it 'can list sites' do
-      VCR.use_cassette('sites') do
-        sites = client.sites.list.to_a
+      sites = client.sites.list.to_a
 
-        expect(sites.length).to eq 2
+      expect(sites.length).to eq 2
 
-        site = sites.find do |s|
-          s['contentUrl'] == 'TestSite'
-        end
-
-        expect(site['id']).to be_a_tableau_id
-        expect(site).to eq(
-          'id' => site['id'],
-          'name' => 'Test Site',
-          'contentUrl' => 'TestSite',
-          'adminMode' => 'ContentAndUsers',
-          'state' => 'Active'
-        )
+      site = sites.find do |s|
+        s['contentUrl'] == 'TestSite'
       end
+
+      expect(site['id']).to be_a_tableau_id
+      expect(site).to eq(
+        'id' => site['id'],
+        'name' => 'Test Site',
+        'contentUrl' => 'TestSite',
+        'adminMode' => 'ContentAndUsers',
+        'state' => 'Active'
+      )
     end
   end
 end
