17bd0f7fce05362f6723e57b78a1e074a0dedd15
Matt Brennan <mbrennan@civisanalytics.com>
2016-09-02 15:37:22 -0400

    workbooks: fix group permission deletion spec

--- spec/resources/workbooks_spec.rb
+++ spec/resources/workbooks_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe TableauApi::Resources::Workbooks do
+describe TableauApi::Resources::Workbooks, vcr: { cassette_name: 'workbooks' } do
   include_context 'tableau client'
 
   def find_or_publish_workbook(name)
@@ -28,45 +28,41 @@ describe TableauApi::Resources::Workbooks do
   #  - http://kb.tableau.com/articles/knowledgebase/desktop-and-server-compatibility
   describe '.publish' do
     it 'can publish a twbx workbook in project in a site' do
-      VCR.use_cassette('workbooks') do
-        workbook_name = 'testpublish'
-        workbook = find_or_publish_workbook(workbook_name)
+      workbook_name = 'testpublish'
+      workbook = find_or_publish_workbook(workbook_name)
 
-        expect(workbook['id']).to be_a_tableau_id
-        expect(workbook['owner']['id']).to be_a_tableau_id
+      expect(workbook['id']).to be_a_tableau_id
+      expect(workbook['owner']['id']).to be_a_tableau_id
 
-        expect(workbook).to eq(
-          'id' => workbook['id'],
-          'name' => workbook_name,
-          'contentUrl' => workbook_name,
-          'showTabs' => 'false',
-          'project' => workbook['project'],
-          'owner' => workbook['owner'],
-          'tags' => "\n    ",
-          'views' => workbook['views']
-        )
-      end
+      expect(workbook).to eq(
+        'id' => workbook['id'],
+        'name' => workbook_name,
+        'contentUrl' => workbook_name,
+        'showTabs' => 'false',
+        'project' => workbook['project'],
+        'owner' => workbook['owner'],
+        'tags' => "\n    ",
+        'views' => workbook['views']
+      )
     end
 
     it 'raises an exception' do
-      VCR.use_cassette('workbooks') do
-        ex = expect do
-          client.workbooks.publish(
-            name: 'tableau_api test',
-            project_id: 'foo',
-            file: 'spec/fixtures/workbooks/test.twbx',
-            overwrite: true
-          )
-        end
+      ex = expect do
+        client.workbooks.publish(
+          name: 'tableau_api test',
+          project_id: 'foo',
+          file: 'spec/fixtures/workbooks/test.twbx',
+          overwrite: true
+        )
+      end
 
-        ex.to raise_error do |e|
-          expect(e).to be_a TableauApi::TableauError
-          expect(e.message).to eq 'Resource Not Found'
-          expect(e.http_response_code).to eq '404'
-          expect(e.error_code).to eq '404005'
-          expect(e.summary).to eq 'Resource Not Found'
-          expect(e.detail).to eq "Project 'foo' could not be found."
-        end
+      ex.to raise_error do |e|
+        expect(e).to be_a TableauApi::TableauError
+        expect(e.message).to eq 'Resource Not Found'
+        expect(e.http_response_code).to eq '404'
+        expect(e.error_code).to eq '404005'
+        expect(e.summary).to eq 'Resource Not Found'
+        expect(e.detail).to eq "Project 'foo' could not be found."
       end
     end
   end
@@ -74,29 +70,24 @@ describe TableauApi::Resources::Workbooks do
   describe '#permissions' do
     # http://onlinehelp.tableau.com/v9.0/api/rest_api/en-us/help.htm#REST/rest_api_ref.htm#Add_Workbook_Permissions%3FTocPath%3DAPI%2520Reference%7C_____9
     it 'can add user permissions to a workbook' do
-      VCR.use_cassette('workbooks') do
-        workbook = find_or_publish_workbook('testpublish')
-        expect(client.workbooks.permissions(
-                 workbook_id: workbook['id'],
-                 user_id: 'e408f778-3708-4685-b7f9-100b584a02aa',
-                 capabilities: { Read: true, ChangePermissions: false }
-        )).to be true
-      end
+      workbook = find_or_publish_workbook('testpublish')
+      expect(client.workbooks.permissions(
+               workbook_id: workbook['id'],
+               user_id: 'e408f778-3708-4685-b7f9-100b584a02aa',
+               capabilities: { Read: true, ChangePermissions: false }
+      )).to be true
     end
 
     # http://onlinehelp.tableau.com/v9.0/api/rest_api/en-us/help.htm#REST/rest_api_ref.htm#Add_Workbook_Permissions%3FTocPath%3DAPI%2520Reference%7C_____9
     it 'can add group permissions to a workbook' do
-      VCR.use_cassette('workbooks') do
-        workbook = find_or_publish_workbook('testpublish')
-        group = client.groups.list.find do |g|
-          g['name'] == 'testgroup'
-        end
-        expect(client.workbooks.permissions(
-                 workbook_id: workbook['id'],
-                 group_id: group['id'],
-                 capabilities: { Read: true, ChangePermissions: false }
-        )).to be true
-      end
+      workbook = find_or_publish_workbook('testpublish')
+      group = client.groups.list.find { |g| g['name'] == 'testgroup' }
+      puts group['id']
+      expect(client.workbooks.permissions(
+               workbook_id: workbook['id'],
+               group_id: group['id'],
+               capabilities: { Read: true, ChangePermissions: false }
+      )).to be true
     end
 
     it 'requires a user or a group id' do
@@ -122,37 +113,25 @@ describe TableauApi::Resources::Workbooks do
 
   describe '#delete_permissions' do
     it 'can delete a user permission' do
-      VCR.use_cassette('workbooks') do
-        workbook = find_or_publish_workbook('testpublish')
-        expect(client.workbooks.permissions(
-                 workbook_id: workbook['id'],
-                 user_id: 'e408f778-3708-4685-b7f9-100b584a02aa',
-                 capabilities: { Read: true }
-        )).to be true
-        expect(client.workbooks.delete_permissions(
-                 workbook_id: workbook['id'],
-                 user_id: 'e408f778-3708-4685-b7f9-100b584a02aa',
-                 capability: 'Read',
-                 capability_mode: 'ALLOW'
-        )).to be true
-      end
+      workbook = find_or_publish_workbook('testpublish')
+      expect(client.workbooks.delete_permissions(
+               workbook_id: workbook['id'],
+               user_id: 'e408f778-3708-4685-b7f9-100b584a02aa',
+               capability: 'Read',
+               capability_mode: 'ALLOW'
+      )).to be true
     end
 
     it 'can delete a group permission' do
-      VCR.use_cassette('workbooks') do
-        workbook = find_or_publish_workbook('testpublish')
-        expect(client.workbooks.permissions(
-                 workbook_id: workbook['id'],
-                 user_id: 'e408f778-3708-4685-b7f9-100b584a02aa',
-                 capabilities: { Read: true }
-        )).to be true
-        expect(client.workbooks.delete_permissions(
-                 workbook_id: workbook['id'],
-                 user_id: 'e408f778-3708-4685-b7f9-100b584a02aa',
-                 capability: 'Read',
-                 capability_mode: 'ALLOW'
-        )).to be true
-      end
+      workbook = find_or_publish_workbook('testpublish')
+      group = client.groups.list.find { |g| g['name'] == 'testgroup' }
+      puts group['id']
+      expect(client.workbooks.delete_permissions(
+               workbook_id: workbook['id'],
+               group_id: group['id'],
+               capability: 'Read',
+               capability_mode: 'ALLOW'
+      )).to be true
     end
 
     it 'requires a user or a group id' do
@@ -180,74 +159,64 @@ describe TableauApi::Resources::Workbooks do
 
   # http://onlinehelp.tableau.com/v9.0/api/rest_api/en-us/help.htm#REST/rest_api_ref.htm#Update_Workbook%3FTocPath%3DAPI%2520Reference%7C_____59
   it 'can update the workbook' do
-    VCR.use_cassette('workbooks') do
-      workbook = find_or_publish_workbook('testpublish')
-      expect(client.workbooks.update(
-               workbook_id: workbook['id'],
-               owner_user_id: 'e408f778-3708-4685-b7f9-100b584a02aa'
-      )).to be true
-    end
+    workbook = find_or_publish_workbook('testpublish')
+    expect(client.workbooks.update(
+             workbook_id: workbook['id'],
+             owner_user_id: 'e408f778-3708-4685-b7f9-100b584a02aa'
+    )).to be true
   end
 
   describe '.list' do
     it 'can list workbooks' do
-      VCR.use_cassette('workbooks') do
-        workbook = find_or_publish_workbook('testpublish')
+      workbook = find_or_publish_workbook('testpublish')
 
-        found_workbook = client.workbooks.list.find do |w|
-          w['contentUrl'] == workbook['contentUrl']
-        end
-
-        expect(found_workbook['id']).to be_a_tableau_id
-        expect(found_workbook).to eq(
-          'id' => workbook['id'],
-          'name' => workbook['name'],
-          'contentUrl' => workbook['contentUrl'],
-          'showTabs' => 'false',
-          'project' => workbook['project'],
-          'owner' => workbook['owner'],
-          'tags' => "\n      "
-        )
-
-        same_workbook = client.workbooks.list.find do |w|
-          w['name'] == workbook['name']
-        end
-
-        expect(found_workbook).to eq same_workbook
+      found_workbook = client.workbooks.list.find do |w|
+        w['contentUrl'] == workbook['contentUrl']
       end
+
+      expect(found_workbook['id']).to be_a_tableau_id
+      expect(found_workbook).to eq(
+        'id' => workbook['id'],
+        'name' => workbook['name'],
+        'contentUrl' => workbook['contentUrl'],
+        'showTabs' => 'false',
+        'project' => workbook['project'],
+        'owner' => workbook['owner'],
+        'tags' => "\n      "
+      )
+
+      same_workbook = client.workbooks.list.find do |w|
+        w['name'] == workbook['name']
+      end
+
+      expect(found_workbook).to eq same_workbook
     end
 
     it 'can paginate workbooks' do
-      VCR.use_cassette('workbooks') do
-        find_or_publish_workbook('test pagination1')
-        find_or_publish_workbook('test pagination2')
-        find_or_publish_workbook('test pagination3')
-        workbooks = client.workbooks.list
+      find_or_publish_workbook('test pagination1')
+      find_or_publish_workbook('test pagination2')
+      find_or_publish_workbook('test pagination3')
+      workbooks = client.workbooks.list
 
-        url = "sites/#{client.auth.site_id}/users/#{client.auth.user_id}/workbooks"
-        expect(client.connection.api_get_collection(url, 'workbooks.workbook', page_size: 2).count).to eq workbooks.count
-      end
+      url = "sites/#{client.auth.site_id}/users/#{client.auth.user_id}/workbooks"
+      expect(client.connection.api_get_collection(url, 'workbooks.workbook', page_size: 2).count).to eq workbooks.count
     end
   end
 
   describe '.views' do
     it 'can get the list of views for a workbook' do
-      VCR.use_cassette('workbooks') do
-        workbook = find_or_publish_workbook('testpublish')
-        views = client.workbooks.views(workbook['id']).to_a
-        expect(views).to eq([workbook['views']['view']])
-      end
+      workbook = find_or_publish_workbook('testpublish')
+      views = client.workbooks.views(workbook['id']).to_a
+      expect(views).to eq([workbook['views']['view']])
     end
   end
 
   describe '.find' do
     it 'can find a workbook by workbook_id' do
-      VCR.use_cassette('workbooks') do
-        workbook = find_or_publish_workbook('testpublish')
-        found_workbook = client.workbooks.find(workbook['id'])
+      workbook = find_or_publish_workbook('testpublish')
+      found_workbook = client.workbooks.find(workbook['id'])
 
-        expect(workbook).to eq found_workbook
-      end
+      expect(workbook).to eq found_workbook
     end
   end
 
