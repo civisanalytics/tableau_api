e28d62d4e32b5c49b990e9af80063ea49e1d9672
timur <timur.tarasenko@gmail.com>
2016-11-08 15:43:12 +0300

    fixed spec files
    fixed spec files

--- lib/tableau_api/resources/datasources.rb
+++ lib/tableau_api/resources/datasources.rb
@@ -1,10 +1,10 @@
 module TableauApi
   module Resources
-     class Datasources < Base
-       def list
-         url = "sites/#{@client.auth.site_id}/datasources"
-         @client.connection.api_get_collection(url, 'datasources.datasource')
-       end
-     end
-   end
+    class Datasources < Base
+      def list
+        url = "sites/#{@client.auth.site_id}/datasources"
+        @client.connection.api_get_collection(url, 'datasources.datasource')
+      end
+    end
+  end
 end
