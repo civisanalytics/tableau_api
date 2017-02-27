fe39c640dd87eb6096d5c658bc8b148bbd2a1ba6
jcobian <jcobian@civisanalytics.com>
2017-02-27 12:30:51 -0600

    Sites: create and delete

--- lib/tableau_api/resources/sites.rb
+++ lib/tableau_api/resources/sites.rb
@@ -4,6 +4,40 @@ module TableauApi
       def list
         @client.connection.api_get_collection('sites', 'sites.site')
       end
+
+      def create(name:, content_url:, admin_mode: nil, num_users: nil, storage_quota: nil)
+        # required parameters
+        request_hash = {
+          name: name,
+          contentUrl: content_url
+        }
+        # optional parameters
+        request_hash[:admin_mode] = admin_mode if admin_mode
+        request_hash[:num_users] = num_users if num_users
+        request_hash[:storage_quota] = storage_quota if storage_quota
+
+        request = Builder::XmlMarkup.new.tsRequest do |ts|
+          ts.site(request_hash)
+        end
+
+        res = @client.connection.api_post('sites', body: request)
+
+        if res.code == 201
+          res['tsResponse']['site'] if res.code == 201
+        else
+          raise TableauError, res
+        end
+
+      end
+
+      def delete(site_id:)
+        res = @client.connection.api_delete("sites/#{site_id}")
+        if res.code == 204
+          true
+        else
+          raise TableauError, res
+        end
+      end
     end
   end
 end
--- spec/resources/sites_spec.rb
+++ spec/resources/sites_spec.rb
@@ -3,12 +3,35 @@ require 'spec_helper'
 describe TableauApi::Resources::Sites, vcr: { cassette_name: 'sites' } do
   include_context 'tableau client'
 
+  describe '#create' do
+    let(:default_client) do
+      TableauApi.new(
+        host: ENV['TABLEAU_HOST'],
+        site_name: 'Default',
+        username: ENV['TABLEAU_ADMIN_USERNAME'],
+        password: ENV['TABLEAU_ADMIN_PASSWORD']
+      )
+    end
+
+    it 'can create a site' do
+      site = default_client.sites.create(name: 'Test Site 2', content_url: 'TestSite2', admin_mode: 'ContentAndUsers')
+      expect(site).not_to be_nil
+      expect(site['id']).to be_a_tableau_id
+      expect(site).to eq(
+        'id' => site['id'],
+        'name' => 'Test Site 2',
+        'contentUrl' => 'TestSite2',
+        'adminMode' => 'ContentAndUsers',
+        'state' => 'Active',
+        'disableSubscriptions' => 'true'
+      )
+    end
+  end
+
   describe '#list' do
     # http://onlinehelp.tableau.com/v9.0/api/rest_api/en-us/help.htm#REST/rest_api_ref.htm#Query_Sites%3FTocPath%3DAPI%2520Reference%7C_____40
     it 'can list sites' do
       sites = client.sites.list.to_a
-
-      expect(sites.length).to eq 2
 
       site = sites.find do |s|
         s['contentUrl'] == 'TestSite'
@@ -22,6 +45,24 @@ describe TableauApi::Resources::Sites, vcr: { cassette_name: 'sites' } do
         'adminMode' => 'ContentAndUsers',
         'state' => 'Active'
       )
+    end
+  end
+
+  describe '#delete' do
+    let(:site_client) do
+      TableauApi.new(
+        host: ENV['TABLEAU_HOST'],
+        site_name: 'TestSite2',
+        username: ENV['TABLEAU_ADMIN_USERNAME'],
+        password: ENV['TABLEAU_ADMIN_PASSWORD']
+      )
+    end
+
+    it 'can delete a site' do
+      site = site_client.sites.list.to_a.find do |s|
+        s['name'] == 'Test Site 2'
+      end
+      expect(site_client.sites.delete(site_id: site['id'])).to be true
     end
   end
 end
