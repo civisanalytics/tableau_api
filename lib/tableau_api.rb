5216d218a47d55b9cd5616860b9c1d3065eafcda
Timur Tarasenko <timur.tarasenko@gmail.com>
2017-07-13 23:49:42 +0300

    added the ability to obtain a list of datasources (#5)
    * added the ability to obtain a list of datasources
    
    added the ability to obtain a list of datasources
    
    * Revert "added the ability to obtain a list of datasources"
    
    This reverts commit 46d641989d5ccf3fb92f0c4dca8635d5ca8109ef.
    
    * removing unnecessary files
    
    * change client.rb
    
    * Added a spec
    
    Added a spec with a corresponding VCR cassette
    
    * fixed spec files
    
    fixed spec files
    
    * changed rubocop.yml
    
    changed rubocop.yml
    
    * fixed for rubocop
    
    fixed for rubocop
    
    * change rubocop.yml
    
    change rubocop.yml
    
    * fix

--- lib/tableau_api.rb
+++ lib/tableau_api.rb
@@ -12,6 +12,7 @@ require 'tableau_api/resources/sites'
 require 'tableau_api/resources/users'
 require 'tableau_api/resources/groups'
 require 'tableau_api/resources/workbooks'
+require 'tableau_api/resources/datasources'
 require 'tableau_api/version'
 
 module TableauApi
